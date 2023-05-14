85b91ee81feb17b86e6291fea9f0dfc4000646d3
Василий Петухов <vonono.weewsa@yandex.ru>
2023-05-14 16:49:59 +0300

    additional

---
+++ WebApplicationRIGO/Models/Passenger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplicationRIGO.Models;
+
+public partial class Passenger
+{
+    public int TripId { get; set; }
+
+    public int UserId { get; set; }
+
+    public int Id { get; set; }
+}
